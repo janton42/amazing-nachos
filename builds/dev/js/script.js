20a8e347c3134c8249676dc33f00a29063ddbdf0
Jeff Stock <jantonstock@gmail.com>
2017-08-02 22:00:44 -0700

    fixed anchors by adding span above header to anchor the #home tag.

--- builds/dev/js/script.js
+++ builds/dev/js/script.js
@@ -3,23 +3,40 @@ $ = require("jquery");
 
 
 
-const nav_btn = document.querySelectorAll(".section-nav");
-console.log(nav_btn);
-
-function set_target() {
-	
-}
+// function target_align() {
+// 	window.scrollBy(0, -100);
+// 	console.log("wtf");
+// }
 
 
-function go_to_target(target) {
-
-	for(var btn of nav_btn) {
-
-	}
-	console.log("you clicked a nav button!");
-}
 
 
+
+// document.getElementById("section-nav1").addEventListener("click", target_align, false);
+// document.getElementById("section-nav2").addEventListener("click", target_align, false);
+
+// // const nav2 = document.getElementById("section-nav2");
+// const nav3 = document.getElementById("section-nav3");
+// const nav4 = document.getElementById("section-nav4");
+// const nav5 = document.getElementById("section-nav5");
+
+// var nav_btns = [nav3,nav4,nav5]
+
+// // // for(i=0; nav_btns.length <= 5; i++)
+
+
+// console.log(nav_btns);
+
+
+
+
+//define function target_align() {
+
+//	console.log("making progress..");
+//}
+
+
+//add element.eventHandler("click", target_align, false); to each element
 },{"jquery":2}],2:[function(require,module,exports){
 /*!
  * jQuery JavaScript Library v3.2.1
--- components/js/target_align.js
+++ components/js/target_align.js
@@ -1,17 +1,35 @@
 
-const nav_btn = document.querySelectorAll(".section-nav");
-console.log(nav_btn);
-
-function set_target() {
-	
-}
+// function target_align() {
+// 	window.scrollBy(0, -100);
+// 	console.log("wtf");
+// }
 
 
-function go_to_target(target) {
 
-	for(var btn of nav_btn) {
 
-	}
-	console.log("you clicked a nav button!");
-}
 
+// document.getElementById("section-nav1").addEventListener("click", target_align, false);
+// document.getElementById("section-nav2").addEventListener("click", target_align, false);
+
+// // const nav2 = document.getElementById("section-nav2");
+// const nav3 = document.getElementById("section-nav3");
+// const nav4 = document.getElementById("section-nav4");
+// const nav5 = document.getElementById("section-nav5");
+
+// var nav_btns = [nav3,nav4,nav5]
+
+// // // for(i=0; nav_btns.length <= 5; i++)
+
+
+// console.log(nav_btns);
+
+
+
+
+//define function target_align() {
+
+//	console.log("making progress..");
+//}
+
+
+//add element.eventHandler("click", target_align, false); to each element
